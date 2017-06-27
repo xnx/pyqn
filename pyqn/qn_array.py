016fb3388bc483ccfe51e60d4627dcf9a901e420
mayya-bondarevskaya <mayya.bondarevskaya@gmail.com>
2017-06-27 13:10:33 +0100

    add basic initialisation of qnArray class

--- pyqn/qn_array.py
+++ pyqn/qn_array.py
@@ -1,5 +1,6 @@
 from .symbol import Symbol
 import numpy as np
+from .quantity import Quantity
 
 class qnArrayError(Exception):
     def __init__(self, error_str):
@@ -8,12 +9,25 @@ class qnArrayError(Exception):
         return self.error_str
 
 class qnArray(Symbol):
-    def __init__(self, name=None, latex=None, html=None, values=None,
-                 units=None, sd=None, definition=None)
+    def __init__(self, name=None, latex=None, html=None, values=None, units=None, sd=None, definition=None):
         Symbol.__init__(self, name, latex, html, definition)
-        if type(values)==list:
-            self.values = np.array(values)
-        elif type(values)==numpy.ndarray:
-            self.values = values
-        else:
-            raise qnArrayError
+        #if type(values)==list:
+        #    self.values = np.array(values)
+        #elif type(values)==numpy.ndarray:
+        #    self.values = values
+        #else:
+        #    raise qnArrayError
+        self.nparr = np.array([])
+        self.units = units
+        self.values = values
+        for v in values:
+            self.nparr = np.append(self.nparr, Quantity(value=v, units = units))
+            
+    def __mul__(self, other):
+        return self.nparr * other
+
+    def values(self):
+        return self.values
+    
+    def units(self):
+        return self.units
