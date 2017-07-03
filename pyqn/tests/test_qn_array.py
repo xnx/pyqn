d35519522d3ca824f786807e688564f1f57e036e
mayya-bondarevskaya <mayya.bondarevskaya@gmail.com>
2017-07-03 10:35:06 +0100

    add multiplication test

--- pyqn/tests/test_qn_array.py
+++ pyqn/tests/test_qn_array.py
@@ -5,24 +5,41 @@ import numpy as np
 
 class qnArrayTest(unittest.TestCase):
     def test_qnarray_init(self):
-        a1 = [1,2,3,4,5]
+        a1 = [-1,-2,-3,-4,-5]
         qnarr1 = qnArray(values = a1, units = 'm')
         for i in range(len(a1)):
             self.assertEqual(qnarr1.values[i], a1[i])
-            self.assertEqual(qnarr1.nparr[i], Quantity(value=a1[i], units='m'))
-        self.assertEqual(qnarr1.units, 'm')
+            self.assertEqual(qnarr1.nparr[i], a1[i])
+        self.assertEqual(qnarr1.units_str, 'm')
         
-        a2 = [-5,-10]
+        a2 = [5,10]
         qnarr2 = qnArray(values = np.array(a2), units = 'J')
         for i in range(len(a2)):
             self.assertEqual(qnarr2.values[i], a2[i])
-            self.assertEqual(qnarr2.nparr[i], Quantity(value=a2[i], units='J'))
-        self.assertEqual(qnarr2.units, 'J')
+            self.assertEqual(qnarr2.nparr[i], a2[i])
+        self.assertEqual(qnarr2.units_str, 'J')
         
         with self.assertRaises(qnArrayError) as e1:
             qnarr = qnArray(values = "string")
         with self.assertRaises(qnArrayError) as e2:
             qnarr = qnArray(values = ["a", "b", "c"])
+            
+    def test_qnarray_mult(self):
+        a1 = [-1,-2,-3,-4,-5]
+        qnarr1 = qnArray(values = a1, units = 'm')
+        q1 = Quantity(value = 2, units = 's')
+        result1 = qnarr1*q1
+        self.assertEqual(result1.units_str, 'm.s')
+        for i in range(len(a1)):
+            self.assertEqual(result1.values[i], 2*a1[i])
+            
+        #a2 = [5,10]
+        #qnarr2 = qnArray(values = np.array(a2), units = 'J')
+        #q2 = Quantity(value=0.2, units = 'm-1')
+        #result2 = qnarr2*q2
+        #self.assertEqual(result2.units, 'J.m-1')
+        #for i in range(len(result2)):
+        #    self.assertEqual(result2.values[i], 0.2*a2[i])
             
 if __name__ == '__main__':
     unittest.main()
