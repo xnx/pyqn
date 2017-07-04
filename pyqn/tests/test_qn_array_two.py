33868f3010c998d495028f69139a2abf1460a4c7
mayya-bondarevskaya <mayya.bondarevskaya@gmail.com>
2017-07-04 11:30:03 +0100

    add function in qnArrayTwo tested

--- pyqn/tests/test_qn_array_two.py
+++ pyqn/tests/test_qn_array_two.py
@@ -1,22 +1,36 @@
 import unittest
-from ..qn_array_two import qnArrayTwo
+from ..qn_array_two import qnArrayTwo, qnArrayTwoError
 from ..units import Units
 
 class qnArrayTwoTest(unittest.TestCase):
-    def qn_array_two_init(self):
+    def test_qn_array_two_init(self):
         vals1 = [1,2,3,4]
         qnarr1 = qnArrayTwo(vals1,units='m')
         self.assertEqual(qnarr1.units, Units('m'))
         for i in range(len(vals1)):
-            self.assertEqual(qnarr1.values[i], vals1[i])
+            self.assertEqual(qnarr1[i], vals1[i])
             
         vals2 = [-10,-20,-30,0,5]
         sd2 = [0.1,0.2,0.3,0.4,0.1]
         qnarr2 = qnArrayTwo(vals2, units = 'J', sd = sd2)
         self.assertEqual(qnarr2.units, Units('J'))
         for i in range(len(vals2)):
-            self.assertEqual(qnarr2.values[i], vals2[i])
+            self.assertEqual(qnarr2[i], vals2[i])
             self.assertEqual(qnarr2.sd[i], sd2[i])
+
+        with self.assertRaises(qnArrayTwoError) as e:
+            qnarr = qnArrayTwo(vals2, units='m', sd = vals1)
+
+    def test_qn_array_two_add(self):
+        vals1 = [1,2,3,4]
+        qnarr1 = qnArrayTwo(vals1,units='m')
+        
+        vals2 = [2,3,4,1]
+        qnarr2 = qnArrayTwo(vals2, units='m')
+        
+        qnarr3 = qnarr1 + qnarr2
+        for i in range(len(vals2)):
+            self.assertEqual(qnarr3[i], vals1[i]+vals2[i])
 
 if __name__ == '__main__':
     unittest.main()
