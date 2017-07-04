98d39d2e85642bc92be086d89d5224883ebfb71f
mayya-bondarevskaya <mayya.bondarevskaya@gmail.com>
2017-07-04 10:37:50 +0100

    add testing of standard deviation initialisation in qnArrayTwo

--- pyqn/tests/test_qn_array_two.py
+++ pyqn/tests/test_qn_array_two.py
@@ -5,10 +5,18 @@ from ..units import Units
 class qnArrayTwoTest(unittest.TestCase):
     def qn_array_two_init(self):
         vals1 = [1,2,3,4]
-        qnarr = qnArrayTwo(vals1,units='m')
-        self.assertEqual(qnarr.units, Units('m'))
+        qnarr1 = qnArrayTwo(vals1,units='m')
+        self.assertEqual(qnarr1.units, Units('m'))
         for i in range(len(vals1)):
-            self.assertEqual(qnarr.values[i], vals1[i])
+            self.assertEqual(qnarr1.values[i], vals1[i])
+            
+        vals2 = [-10,-20,-30,0,5]
+        sd2 = [0.1,0.2,0.3,0.4,0.1]
+        qnarr2 = qnArrayTwo(vals2, units = 'J', sd = sd2)
+        self.assertEqual(qnarr2.units, Units('J'))
+        for i in range(len(vals2)):
+            self.assertEqual(qnarr2.values[i], vals2[i])
+            self.assertEqual(qnarr2.sd[i], sd2[i])
 
 if __name__ == '__main__':
     unittest.main()
