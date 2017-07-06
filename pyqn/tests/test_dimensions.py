3986d7266679f4e2aeb06bf7db3b658bbff87cda
mayya-bondarevskaya <mayya.bondarevskaya@gmail.com>
2017-07-06 12:33:22 +0100

    add tests for usage of DimensionsError class and when it arises furing initialisation

--- pyqn/tests/test_dimensions.py
+++ pyqn/tests/test_dimensions.py
@@ -1,11 +1,26 @@
 import unittest
-from ..dimensions import Dimensions
+from ..dimensions import Dimensions, DimensionsError
 
 class DimensionsCheck(unittest.TestCase):
     def test_dimensions_init(self):
-        d1 = Dimensions(dims = [1,0,0,0,0,0,1])
-        d2 = Dimensions(dims = [0,0,0,0,0,0,0])
-        d3 = Dimensions(dims = [1,1,1,1,1,1,1])
+        arr1 = [1,0,0,0,0,0,1]
+        arr2 = [0,0,0,0,0,0,0]
+        arr3 = [1,1,1,1,1,1,1]
+        d1 = Dimensions(dims = arr1)
+        d2 = Dimensions(dims = arr2)
+        d3 = Dimensions(dims = arr3)
+        d4 = Dimensions(dims = [])
+
+        for i in range(7):
+            self.assertEqual(d1.dims[i], arr1[i])
+            self.assertEqual(d2.dims[i], arr2[i])
+            self.assertEqual(d3.dims[i], arr3[i])
+            self.assertEqual(d4.dims[i], 0)
+
+        with self.assertRaises(DimensionsError) as e:
+            d = Dimensions(dims = [1])
+        with self.assertRaises(DimensionsError) as e:
+            d = Dimensions(dims = [1,1,1,1,1,1,1,1,1,1,1])
     
 if __name__ == '__main__':
     unittest.main()
