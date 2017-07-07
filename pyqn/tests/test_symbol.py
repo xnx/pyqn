4a0b43e33c0b64257d197a794c66b7840f50074b
mayya-bondarevskaya <mayya.bondarevskaya@gmail.com>
2017-07-07 11:19:39 +0100

    add test for symbol class init

---
+++ pyqn/tests/test_symbol.py
@@ -0,0 +1,38 @@
+import unittest
+from ..symbol import Symbol
+
+class SymbolCheck(unittest.TestCase):
+    def test_symbol_init(self):
+        s = Symbol()
+        self.assertTrue(s.name == None)
+        self.assertTrue(s.latex == None)
+        self.assertTrue(s.html == None)
+        self.assertTrue(s.definition == None)
+        
+        s = Symbol(name = 'NAME')
+        self.assertTrue(s.name == 'NAME')
+        self.assertTrue(s.latex == 'NAME')
+        self.assertTrue(s.html == 'NAME')
+        self.assertTrue(s.definition == None)
+        
+        s = Symbol(name = 'NAME', html = 'HTML')
+        self.assertTrue(s.name == 'NAME')
+        self.assertTrue(s.html == 'HTML')
+        self.assertTrue(s.latex == 'NAME')
+        self.assertTrue(s.definition == None)
+        
+        s = Symbol(definition = 'DEFINITION')
+        self.assertTrue(s.name == None)
+        self.assertTrue(s.html == None)
+        self.assertTrue(s.latex == None)
+        self.assertTrue(s.definition == 'DEFINITION')
+        
+        s = Symbol(name = 'NAME', latex = 'LATEX')
+        self.assertTrue(s.name == 'NAME')
+        self.assertTrue(s.html == 'NAME')
+        self.assertTrue(s.latex == 'LATEX')
+        self.assertTrue(s.definition == None)
+
+if __name__ == '__main__':
+    unittest.main()
+
