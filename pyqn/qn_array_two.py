79b9ace31d358e390ce6a172462afc8a91454a38
mayya-bondarevskaya <mayya.bondarevskaya@gmail.com>
2017-07-12 11:14:41 +0100

    add radd, rsub, rmul, and rtruediv functions to qnArrayTwo class

--- pyqn/qn_array_two.py
+++ pyqn/qn_array_two.py
@@ -40,6 +40,9 @@ class qnArrayTwo(np.ndarray):
             return qnArrayTwo(super(qnArrayTwo, self).__add__(other.value), units = self.units)
         return qnArrayTwo(super(qnArrayTwo, self).__add__(other), units = self.units)
         
+    def __radd__(self, other):
+        return self.__add__(other)
+        
     def __sub__(self, other):
         if type(other) is qnArrayTwo:
             if self.units != other.units:
@@ -51,12 +54,26 @@ class qnArrayTwo(np.ndarray):
             return qnArrayTwo(super(qnArrayTwo, self).__sub__(other.value), units = self.units)
         return qnArrayTwo(super(qnArrayTwo, self).__sub__(other), units = self.units)
             
+    def __rsub__(self, other):
+        if type(other) is qnArrayTwo:
+            if self.units != other.units:
+                raise qnArrayTwoError('Units of the two array must be compatible')
+            return qnArrayTwo(super(qnArrayTwo, self).__rsub__(super(qnArrayTwo, other)), units = self.units)
+        if type(other) is Quantity:
+            if self.units != other.units:
+                raise qnArrayTwoError('Units of the two array must be compatible')
+            return qnArrayTwo(super(qnArrayTwo, self).__rsub__(other.value), units = self.units)
+        return qnArrayTwo(super(qnArrayTwo, self).__rsub__(other), units = self.units)
+            
     def __mul__(self, other):
         if type(other) is qnArrayTwo:
             return qnArrayTwo(super(qnArrayTwo, self).__mul__(super(qnArrayTwo, other)), units = self.units * other.units)
         if type(other) is Quantity:
             return qnArrayTwo(super(qnArrayTwo, self).__mul__(other.value), units = self.units * other.units)
         return qnArrayTwo(super(qnArrayTwo, self).__mul__(other), units = self.units)
+    
+    def __rmul__(self, other):
+        return self.__mul__(other)
             
     def __truediv__(self, other):
         if type(other) is qnArrayTwo:
@@ -64,6 +81,13 @@ class qnArrayTwo(np.ndarray):
         if type(other) is Quantity:
             return qnArrayTwo(super(qnArrayTwo, self).__truediv__(other.value), units = self.units / other.units)
         return qnArrayTwo(super(qnArrayTwo, self).__truediv__(other), units = self.units)
+        
+    def __rtruediv__(self, other):
+        if type(other) is qnArrayTwo:
+            return qnArrayTwo(super(qnArrayTwo, self).__rtruediv__(super(qnArrayTwo, other)), units = other.units / self.units)
+        if type(other) is Quantity:
+            return qnArrayTwo(super(qnArrayTwo, self).__rtruediv__(other.value), units = other.units / self.units)
+        return qnArrayTwo(super(qnArrayTwo, self).__rtruediv__(other), units = Units('1') / self.units)
         
     def __pow__(self, power):
         return qnArrayTwo(super(qnArrayTwo, self).__pow__(power), units = self.units ** power)
