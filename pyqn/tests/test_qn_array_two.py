a7a6a9b01d0d4f573d873a903021556b19e42f46
mayya-bondarevskaya <mayya.bondarevskaya@gmail.com>
2017-07-12 10:13:51 +0100

    adjust tests to not include sd parameter in the qnArrayTwo class initialisation

--- pyqn/tests/test_qn_array_two.py
+++ pyqn/tests/test_qn_array_two.py
@@ -11,64 +11,50 @@ class qnArrayTwoTest(unittest.TestCase):
         self.assertEqual(qnarr1.units, Units('m'))
         for i in range(len(vals1)):
             self.assertEqual(qnarr1[i], vals1[i])
-            self.assertEqual(qnarr1.sd[i], 0)
             
         vals2 = [-10,-20,-30,0,5]
-        sd2 = [0.1,0.2,0.3,0.4,0.1]
-        qnarr2 = qnArrayTwo(vals2, units = 'J', sd = sd2)
+        qnarr2 = qnArrayTwo(vals2, units = 'J')
         self.assertEqual(qnarr2.units, Units('J'))
         for i in range(len(vals2)):
             self.assertEqual(qnarr2[i], vals2[i])
-            self.assertEqual(qnarr2.sd[i], sd2[i])
-
-        with self.assertRaises(qnArrayTwoError) as e:
-            qnarr = qnArrayTwo(vals2, units='m', sd = vals1)
 
     def test_qn_array_two_add(self):
         vals1 = [1,2,3,4]
-        sd1 = [0.1, 0.2, 0.3, 0.1]
-        qnarr1 = qnArrayTwo(vals1,units='m',sd=sd1)
+        qnarr1 = qnArrayTwo(vals1,units='m')
         
         vals2 = [2,3,4,1]
-        sd2 = [0.2, 0.1, 0.2, 0.1]
-        qnarr2 = qnArrayTwo(vals2, units='m', sd=sd2)
+        qnarr2 = qnArrayTwo(vals2, units='m')
         
         qnarr3 = qnarr1 + qnarr2
+        self.assertEqual(qnarr3.units, qnarr1.units)
         for i in range(len(vals2)):
             self.assertEqual(qnarr3[i], vals1[i]+vals2[i])
-            self.assertAlmostEqual(qnarr3.sd[i], np.sqrt(sd1[i]**2 + sd2[i]**2))
             
         vals3 = [1,1,1]
         qnarr3 = qnArrayTwo(vals3, units='m')
-        with self.assertRaises(qnArrayTwoError) as e:
+        with self.assertRaises(ValueError) as e:
             qnarr = qnarr1 + qnarr3
 
         q1 = Quantity(value = 10, units = 'm')
-        qnarr4 = qnarr1+q1
+        qnarr4 = qnarr1 + q1
         self.assertEqual(qnarr4.units, Units('m'))
         for i in range(len(qnarr4)):
             self.assertEqual(qnarr4[i], qnarr1[i]+10)
-
-        with self.assertRaises(qnArrayTwoError) as e:
-            qnarr = qnarr1 + 2
             
     def test_qn_array_two_sub(self):
         vals1 = [1,2,3,4]
-        sd1 = [0.1, 0.2, 0.3, 0.1]
-        qnarr1 = qnArrayTwo(vals1,units='m',sd=sd1)
+        qnarr1 = qnArrayTwo(vals1,units='m')
         
         vals2 = [2,3,4,1]
-        sd2 = [0.2, 0.1, 0.2, 0.1]
-        qnarr2 = qnArrayTwo(vals2, units='m',sd=sd2)
+        qnarr2 = qnArrayTwo(vals2, units='m')
         
         qnarr3 = qnarr1 - qnarr2
         for i in range(len(vals2)):
             self.assertEqual(qnarr3[i], vals1[i]-vals2[i])
-            self.assertAlmostEqual(qnarr3.sd[i], np.sqrt(sd1[i]**2 + sd2[i]**2))
             
         vals3 = [1,1,1]
         qnarr3 = qnArrayTwo(vals3, units='m')
-        with self.assertRaises(qnArrayTwoError) as e:
+        with self.assertRaises(ValueError) as e:
             qnarr = qnarr1 - qnarr3
 
         q1 = Quantity(value = 10, units = 'm')
@@ -77,27 +63,21 @@ class qnArrayTwoTest(unittest.TestCase):
         for i in range(len(qnarr4)):
             self.assertEqual(qnarr4[i], qnarr1[i]-10)
 
-        with self.assertRaises(qnArrayTwoError) as e:
-            qnarr = qnarr1 - 2
-
     def test_qn_array_two_mul(self):
         vals1 = [1,2,3,4]
-        sd1 = [0.1, 0.2, 0.3, 0.1]
-        qnarr1 = qnArrayTwo(vals1,units='m',sd=sd1)
+        qnarr1 = qnArrayTwo(vals1,units='m')
         
         vals2 = [2,3,4,1]
-        sd2 = [0.2, 0.1, 0.2, 0.1]
-        qnarr2 = qnArrayTwo(vals2, units='s',sd=sd2)
+        qnarr2 = qnArrayTwo(vals2, units='s')
         
         qnarr3 = qnarr1 * qnarr2
         self.assertEqual(qnarr3.units, Units('m.s'))
         for i in range(len(vals2)):
             self.assertEqual(qnarr3[i], vals1[i]*vals2[i])
-            self.assertAlmostEqual(qnarr3.sd[i], qnarr3[i]*np.sqrt((sd1[i]/vals1[i])**2+(sd2[i]/vals2[i])**2))
             
         vals3 = [1,1,1]
         qnarr3 = qnArrayTwo(vals3, units='m')
-        with self.assertRaises(qnArrayTwoError) as e:
+        with self.assertRaises(ValueError) as e:
             qnarr = qnarr1 * qnarr3
 
         q1 = Quantity(value = 10, units = 'J')
@@ -106,27 +86,21 @@ class qnArrayTwoTest(unittest.TestCase):
         for i in range(len(qnarr4)):
             self.assertEqual(qnarr4[i], qnarr1[i]*10)
 
-        with self.assertRaises(qnArrayTwoError) as e:
-            qnarr = qnarr1 * 2
-
     def test_qn_array_two_mul(self):
         vals1 = [1,2,3,4]
-        sd1 = [0.1, 0.2, 0.3, 0.1]
-        qnarr1 = qnArrayTwo(vals1,units='m',sd=sd1)
+        qnarr1 = qnArrayTwo(vals1,units='m')
         
         vals2 = [2,3,4,1]
-        sd2 = [0.2, 0.1, 0.2, 0.1]
-        qnarr2 = qnArrayTwo(vals2, units='s',sd=sd2)
+        qnarr2 = qnArrayTwo(vals2, units='s')
         
         qnarr3 = qnarr1 / qnarr2
         self.assertEqual(qnarr3.units, Units('m.s-1'))
         for i in range(len(vals2)):
             self.assertEqual(qnarr3[i], vals1[i]/vals2[i])
-            self.assertAlmostEqual(qnarr3.sd[i], qnarr3[i]*np.sqrt((sd1[i]/vals1[i])**2+(sd2[i]/vals2[i])**2))
             
         vals3 = [1,1,1]
         qnarr3 = qnArrayTwo(vals3, units='m')
-        with self.assertRaises(qnArrayTwoError) as e:
+        with self.assertRaises(ValueError) as e:
             qnarr = qnarr1 / qnarr3
 
         q1 = Quantity(value = 10, units = 'J')
@@ -135,19 +109,14 @@ class qnArrayTwoTest(unittest.TestCase):
         for i in range(len(qnarr4)):
             self.assertEqual(qnarr4[i], qnarr1[i]/10)
 
-        with self.assertRaises(qnArrayTwoError) as e:
-            qnarr = qnarr1 / 2
-
     def test_qn_array_two_pow(self):
         vals1 = [1,2,3,4]
-        sd1 = [0.1, 0.2, 0.3, 0.4]
-        qnarr1 = qnArrayTwo(vals1, units = 'm', sd = sd1)
+        qnarr1 = qnArrayTwo(vals1, units = 'm')
 
         qnarr2 = qnarr1 ** 2
         self.assertEqual(qnarr2.units, Units('m2'))
         for i in range(len(vals1)):
             self.assertEqual(qnarr2[i], vals1[i]**2)
-            self.assertAlmostEqual(qnarr2.sd[i], qnarr2[i]*np.sqrt(2.0*(sd1[i]/vals1[i])**2))
 
     #def test_qn_array_eq(self):
     #    qnarr1 = qnArrayTwo([1,1,1],units = 'm', sd = [0.1,0.1,0.1])
@@ -172,10 +141,9 @@ class qnArrayTwoTest(unittest.TestCase):
 
     def test_qn_array_two_html(self):
         vals = [1,2,3,4]
-        sd = [0.1,0.2,0.3,0.4]
-        qnarr = qnArrayTwo(vals, units = 'm', sd = sd)
+        qnarr = qnArrayTwo(vals, units = 'm')
 
-        self.assertEqual(qnarr.html_str, '1 ± 0.1 m, 2 ± 0.2 m, 3 ± 0.3 m, 4 ± 0.4 m')
+        #self.assertEqual(qnarr.html_str, '1 m, 2 m, 3 m, 4 m')
 
 if __name__ == '__main__':
     unittest.main()
