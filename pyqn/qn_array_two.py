3fa23b42c0ef3520ab034d222e571cc3b55b66b8
mayya-bondarevskaya <mayya.bondarevskaya@gmail.com>
2017-07-24 13:05:00 +0100

    add unit checking for ufuncs, make append function only accept Quantity objects

--- pyqn/qn_array_two.py
+++ pyqn/qn_array_two.py
@@ -11,8 +11,8 @@ class qnArrayTwoError(Exception):
 
 class qnArrayTwo(np.ndarray):
     def __new__(cls, input_array, info=None, units='1', sd=None):
-        """ Initialises a qnArray as a child class derived from the 
-        numpy ndarray. 
+        """ Initialises a qnArray as a child class derived from the
+        numpy ndarray.
         """
         obj = np.asarray(input_array).view(cls)
         obj.info = info
@@ -20,28 +20,28 @@ class qnArrayTwo(np.ndarray):
             obj.sd = np.zeros(len(input_array))
         else:
             obj.sd = np.array(sd)
-        
+
         if type(units) is str:
             obj.units = Units(units) #records units as Unit class
         elif type(units) is Units:
-            obj.units = units            
+            obj.units = units
         return obj
 
     def __array_finalize__(self, obj):
         if obj is None: return
         self.info = getattr(obj, 'info', None)
         self.sd = getattr(obj, 'sd', None)
-        
+
     def __array_ufunc__(self, ufunc, method, *inputs, **kwargs):
         if ufunc in ufunc_dict_alg:
             alg_func = ufunc_dict_alg[ufunc][0]
             sd_func = ufunc_dict_alg[ufunc][1]
             units_func = ufunc_dict_alg[ufunc][2]
             alg_func_reverse = ufunc_dict_alg[ufunc][3]
-            
+
             if all([hasattr(x, 'units') for x in inputs]):
                 result_units = units_func(inputs[0].units, inputs[1].units)
-                    
+
             # if both are qn arrays
             if (type(inputs[1]) is qnArrayTwo) and (type(inputs[0]) is qnArrayTwo):
                 result_val = getattr(np.asarray(inputs[0]), alg_func)(np.asarray(inputs[1]))
@@ -49,7 +49,7 @@ class qnArrayTwo(np.ndarray):
                                                 np.asarray(inputs[1]),
                                                 inputs[0].sd,
                                                 inputs[1].sd)
-                
+
             # if one input is a quantity
             elif type(inputs[1]) is Quantity:
                 result_val = getattr(np.asarray(inputs[0]), alg_func)(inputs[1].value)
@@ -57,14 +57,14 @@ class qnArrayTwo(np.ndarray):
                                                 inputs[1].value,
                                                 inputs[0].sd,
                                                 inputs[1].sd)
-                
+
             elif type(inputs[0]) is Quantity:
                 result_val = getattr(np.asarray(inputs[1]), alg_func_reverse)(inputs[0].value)
                 result_sd = sd_func(result_val, inputs[0].value,
                                                 np.asarray(inputs[1]),
                                                 inputs[0].sd,
                                                 inputs[1].sd)
-                
+
             # for all other object types
             elif type(inputs[0]) is qnArrayTwo:
                 result_val = getattr(np.asarray(inputs[0]), alg_func)(inputs[1])
@@ -72,7 +72,7 @@ class qnArrayTwo(np.ndarray):
                                                 inputs[1],
                                                 inputs[0].sd, 0)
                 result_units = units_func(inputs[0].units, Units('1'))
-                
+
             else:
                 result_val = getattr(np.asarray(inputs[1]), alg_func_reverse)(inputs[0])
                 result_sd = sd_func(result_val, inputs[0],
@@ -82,13 +82,13 @@ class qnArrayTwo(np.ndarray):
 
             return qnArrayTwo(result_val, units = result_units, sd = result_sd)
         elif ufunc in ufunc_dict_other:
-            if inputs[0].units.has_units() is True:
-                raise qnArrayTwoError('qnArray must be unitless')
-            sd_func = ufunc_dict_other[ufunc]
-            result_val = ufunc(np.asarray(inputs[0]))
-            result_sd = sd_func(result_val, np.asarray(inputs[0]), inputs[0].sd)
+            unit_test_func = ufunc_dict_other[ufunc][1]
+            inputs_checked = unit_test_func(inputs[0])
+            sd_func = ufunc_dict_other[ufunc][0]
+            result_val = ufunc(np.asarray(inputs_checked))
+            result_sd = sd_func(result_val, np.asarray(inputs_checked), inputs_checked.sd)
             return qnArrayTwo(result_val, units = Units('1'), sd = result_sd)
-            
+
         elif ufunc in ufunc_dict_otherother:
             if inputs[0].units.has_units() is True:
                 raise qnArrayTwoError('qnArray must be unitless')
@@ -96,7 +96,7 @@ class qnArrayTwo(np.ndarray):
             result_val = ufunc(np.asarray(inputs[0]),np.asarray(inputs[1]))
             result_sd = sd_func(result_val, np.asarray(inputs[0]), np.asarray(inputs[1]), inputs[0].sd, inputs[1].sd)
             return qnArrayTwo(result_val, units = Units('1'), sd = result_sd)
-            
+
     def __eq__(self, other):
         if all(super(qnArrayTwo, self).__eq__(super(qnArrayTwo, other))) and (self.units == other.units):
             return True
@@ -112,37 +112,31 @@ class qnArrayTwo(np.ndarray):
         for i in range(len(self)):
             html_chunks.append('{} {}'.format(self[i],self.units))
         return ', '.join(html_chunks)
-        
+
     def convert_units_to(self, new_units, force=None):
         to_units = Units(new_units)
         fac = self.units.conversion(to_units, force)
         new_vals = np.asarray(self)*fac
         new_sd = self.sd*fac
         return qnArrayTwo(new_vals, units = new_units, sd = new_sd)
-        
-    def append(self, value = None, sd = 0, *inputs):
-        if type(inputs[0]) is Quantity:
-            if self.units != inputs[0].units:
-                raise qnArrayTwoError('Same units expected')
-            if inputs[0] is not None:
-                return qnArrayTwo(np.append(np.asarray(self),inputs[0].value), units = self.units, sd = np.append(self.sd,inputs[0].sd))
-            else:
-                return qnArrayTwo(np.append(np.asarray(self),inputs[0].value), units = self.units, sd = np.append(self.sd,0))
-        elif value is not None:
-            return qnArrayTwo(np.append(np.asarray(self),value), units = self.units, sd = np.append(self.sd,sd))
-                
+
+    def append(self, input_quantity):
+        if self.units != input_quantity.units:
+            raise qnArrayTwoError('Same units expected')
+        return qnArrayTwo(np.append(np.asarray(self),input_quantity.value), units = self.units, sd = np.append(self.sd,input_quantity.sd))
+       
 def sd_add_sub(result, vals1, vals2, sd1, sd2):
     return np.sqrt(sd1**2+sd2**2)
 def sd_mul_div(result, vals1, vals2, sd1, sd2):
     return result*np.sqrt((sd1/vals1)**2+(sd2/vals2)**2)
-    
+
 def sd_logaddexp(result, vals1, vals2, sd1, sd2):
     return np.sqrt(np.exp(vals1)**2*sd1**2+np.exp(vals2)**2*sd2**2)/(np.exp(vals1)+np.exp(vals2))
 def sd_logaddexp2(result, vals1, vals2, sd1, sd2):
     return np.sqrt(np.exp(vals1)**2*sd1**2+np.exp(vals2)**2*sd2**2)/((np.log(2))*(np.exp(vals1)+np.exp(vals2)))
 def sd_power(result, vals1, vals2, sd1, sd2):
     return np.sqrt(sd1**2*(vals2*vals1**(vals2-1))**2 + sd2**2*(result*np.log(vals1))**2)
-    
+
 def sd_exp(result, vals, sd):
     return result * sd
 def sd_sin(result, vals, sd):
@@ -167,7 +161,21 @@ def sd_arccosh(result, vals, sd):
     return sd/(np.sqrt(vals-1)*np.sqrt(vals+1))
 def sd_arctanh(result, vals, sd):
     return sd/(1-vals**2)
-    
+
+def units_check_unitless(input_arr):
+    if input_arr.units.has_units() is True:
+        raise qnArrayTwoError('qnArray must be unitless')
+    else:
+        return input_arr
+
+def units_check_unitless_deg_rad(input_arr):
+    if input_arr.units == Units('deg'):
+        return input_arr.convert_units_to('rad')
+    elif input_add.units == Units('rad') or input_arr.units.has_units() is True:
+        return input_add
+    else:
+        raise qnArrayTwoError('qnArray must have units: deg, rad, unitless')
+
 def units_add_sub(u1, u2):
     if u1.has_units() is True:
         return u1
@@ -177,26 +185,26 @@ def units_mul(u1,u2):
     return u1*u2
 def units_div(u1,u2):
     return u1/u2
-    
+
 ufunc_dict_alg = {  np.add: ('__add__', sd_add_sub, units_add_sub, '__radd__'),
                     np.subtract: ('__sub__', sd_add_sub, units_add_sub, '__rsub__'),
                     np.multiply: ('__mul__', sd_mul_div, units_mul, '__rmul__'),
                     np.divide: ('__truediv__', sd_mul_div, units_div, '__rtruediv__')}
-                
-ufunc_dict_other = { np.exp: sd_exp,
-                     np.sin: sd_sin,
-                     np.cos: sd_cos,
-                     np.tan: sd_tan,
-                     np.arcsin: sd_arcsin_arccos,
-                     np.arccos: sd_arcsin_arccos,
-                     np.arctan: sd_arctan,
-                     np.sinh: sd_sinh,
-                     np.cosh: sd_cosh,
-                     np.tanh: sd_tanh,
-                     np.arcsinh: sd_arcsinh,
-                     np.arccosh: sd_arccosh,
-                     np.arctanh: sd_arctanh}
-                     
+
+ufunc_dict_other = { np.exp: (sd_exp, units_check_unitless),
+                     np.sin: (sd_sin, units_check_unitless_deg_rad),
+                     np.cos: (sd_cos, units_check_unitless_deg_rad),
+                     np.tan: (sd_tan, units_check_unitless_deg_rad),
+                     np.arcsin: (sd_arcsin_arccos, units_check_unitless),
+                     np.arccos: (sd_arcsin_arccos, units_check_unitless),
+                     np.arctan: (sd_arctan, units_check_unitless),
+                     np.sinh: (sd_sinh, units_check_unitless),
+                     np.cosh: (sd_cosh, units_check_unitless),
+                     np.tanh: (sd_tanh, units_check_unitless),
+                     np.arcsinh: (sd_arcsinh, units_check_unitless),
+                     np.arccosh: (sd_arccosh, units_check_unitless),
+                     np.arctanh: (sd_arctanh, units_check_unitless)}
+
 ufunc_dict_otherother = {np.logaddexp: sd_logaddexp,
                          np.logaddexp2: sd_logaddexp2,
                          np.power: sd_power}
