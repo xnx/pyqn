37e3abeab6be0783e0ecddecae844b0af0e2337e
mayya-bondarevskaya <mayya.bondarevskaya@gmail.com>
2017-07-27 01:35:49 +0100

    rearrange testing mechanism to loop over reworked ufuncs

--- pyqn/tests/test_qn_array_two.py
+++ pyqn/tests/test_qn_array_two.py
@@ -2,6 +2,7 @@ import unittest
 from ..qn_array_two import qnArrayTwo, qnArrayTwoError
 from ..units import Units
 from ..quantity import Quantity
+from ..ufunc_dictionaries import *
 import numpy as np
 
 class qnArrayTwoTest(unittest.TestCase):
@@ -188,45 +189,99 @@ class qnArrayTwoTest(unittest.TestCase):
         sd2 = [0.01,0.02,0.03]
         q1 = qnArrayTwo(a1,units = 'm', sd = sd1)
         q2 = qnArrayTwo(a2,units = 'm', sd = sd2)
-        add = np.add(q1,q2)
+        q3 = qnArrayTwo(a2,units = 's', sd = sd2)
         
+        for ufunc in ufunc_dict_alg:
+            if (ufunc is np.multiply) or (ufunc is np.divide):
+                in1 = q1
+                in2 = q3
+            elif (ufunc is np.add) or (ufunc is np.subtract):
+                in1 = q1
+                in2 = q2
+            result = ufunc(in1, in2)
+            
+            sd_func = ufunc_dict_alg[ufunc][1]
+            units_func = ufunc_dict_alg[ufunc][2]
+            
+            self.assertTrue(result.units, units_func(in1.units, in2.units))
+            for i in range(3):
+                self.assertEqual(result[i], ufunc(np.asarray(in1), np.asarray(in2))[i])
+                self.assertEqual(result.sd[i], sd_func(np.asarray(result), np.asarray(in1), np.asarray(in2), in1.sd, in2.sd)[i])
+                
+            print('{} tested!'.format(ufunc))
+        
+        a1 = [0.1,0.2,0.3]
+        sd1 = [0.01,0.02,0.03]
         q3 = qnArrayTwo(a1, units = '1', sd = sd1)
-        q33 = qnArrayTwo(a2, units = '1', sd = sd2)
-        q4 = np.exp(q3)
-        q5 = np.sin(q3)
-        q6 = np.cos(q3)
-        q7 = np.tan(q3)
-        q8 = np.arcsin(q33)
-        q9 = np.arccos(q33)
-        q10 = np.arctan(q33)
-        q11 = np.sinh(q33)
-        q12 = np.cosh(q33)
-        q13 = np.tanh(q33)
-        q14 = np.logaddexp(q3,q33)
-        q15 = np.power(q3,q33)
-        for i in range(3):
-            self.assertAlmostEqual(q4[i], np.exp(a1[i]))
-            self.assertAlmostEqual(q4.sd[i], q4[i]*q3.sd[i])
-            self.assertAlmostEqual(q5[i], np.sin(a1[i]))
-            self.assertAlmostEqual(q5.sd[i], np.cos(a1[i])*sd1[i])
-            self.assertAlmostEqual(q6[i], np.cos(a1[i]))
-            self.assertAlmostEqual(q6.sd[i], np.sin(a1[i])*sd1[i])
-            self.assertAlmostEqual(q7[i], np.tan(a1[i]))
-            #self.assertAlmostEqual(q7.sd[i], np.cos(a1[i])**(-2)*sd1[i])
-            self.assertAlmostEqual(q8[i], np.arcsin(a2[i]))
-            #self.assertAlmostEqual(q8.sd[i], sd2[i]/np.sqrt(1-sd2[i]**2))
-            self.assertAlmostEqual(q9[i], np.arccos(a2[i]))
-            #self.assertAlmostEqual(q9.sd[i], sd2[i]/np.sqrt(1-sd2[i]**2))
-            self.assertAlmostEqual(q10[i], np.arctan(a2[i]))
-            #self.assertAlmostEqual(q10.sd[i], sd2[i]*np.cosh(a2[i])**(-2))
-            self.assertAlmostEqual(q11[i], np.sinh(a2[i]))
-            self.assertAlmostEqual(q11.sd[i], sd2[i]*np.cosh(a2[i]))
-            self.assertAlmostEqual(q12[i], np.cosh(a2[i]))
-            self.assertAlmostEqual(q12.sd[i], sd2[i]*np.sinh(a2[i]))
-            self.assertAlmostEqual(q13[i], np.tanh(a2[i]))
-            self.assertAlmostEqual(q13.sd[i], sd2[i]*np.cosh(a2[i])**(-2))
-            self.assertAlmostEqual(q14[i], np.logaddexp(a1[i], a2[i]))
-            self.assertAlmostEqual(q15[i], np.power(np.asarray(q3[i]),np.asarray(q33[i])))
+        q4 = qnArrayTwo(a1, units = 'rad', sd = sd1)
+        q5 = qnArrayTwo(a1, units = 'deg', sd = sd1)
+        
+        #~ for ufunc in ufunc_dict_one_input:
+            #~ unit_test = ufunc_dict_one_input[ufunc][1]
+            #~ if unit_test is units_check_unitless:
+                #~ with self.assertRaises(Exception) as e:
+                    #~ ufunc(q1)
+                #~ with self.assertRaises(Exception) as e:
+                    #~ ufunc(q2)
+                #~ with self.assertRaises(Exception) as e:
+                    #~ ufunc(q4)
+                #~ with self.assertRaises(Exception) as e:
+                    #~ ufunc(q5)
+                #~ in1 = [q3]
+                #~ r = [ufunc(q3)]
+            #~ elif unit_test is units_check_unitless_deg_rad:
+                #~ with self.assertRaises(Exception) as e:
+                    #~ ufunc(q1)
+                #~ with self.assertRaises(Exception) as e:
+                    #~ ufunc(q2)
+                #~ in1 = [q3, q4, q5]
+                #~ r = [ufunc(q3), ufunc(q4), ufunc(q5)]
+            #~ sd_func = ufunc_dict_one_input[ufunc][0] #(result, vals, sd)
+            #~ units_func = ufunc_dict_one_input[ufunc][2] #(units)
+            
+            #~ for result, input1 in zip(r,in1):
+                #~ self.assertEqual(result.units, units_func(input1.units))
+                #~ for i in range(3):
+                    #~ self.assertAlmostEqual(result[i], ufunc(np.asarray(input1))[i], places = 1)
+                    #~ self.assertAlmostEqual(result.sd[i], sd_func(result, input1, input1.sd)[i],places = 1)
+            
+            #~ print('{} tested!'.format(ufunc))
+        
+        #~ q4 = np.exp(q3)
+        #~ q5 = np.sin(q3)
+        #~ q6 = np.cos(q3)
+        #~ q7 = np.tan(q3)
+        #~ q8 = np.arcsin(q33)
+        #~ q9 = np.arccos(q33)
+        #~ q10 = np.arctan(q33)
+        #~ q11 = np.sinh(q33)
+        #~ q12 = np.cosh(q33)
+        #~ q13 = np.tanh(q33)
+        #~ q14 = np.logaddexp(q3,q33)
+        #~ q15 = np.power(q3,q33)
+        #~ for i in range(3):
+            #~ self.assertAlmostEqual(q4[i], np.exp(a1[i]))
+            #~ self.assertAlmostEqual(q4.sd[i], q4[i]*q3.sd[i])
+            #~ self.assertAlmostEqual(q5[i], np.sin(a1[i]))
+            #~ self.assertAlmostEqual(q5.sd[i], np.cos(a1[i])*sd1[i])
+            #~ self.assertAlmostEqual(q6[i], np.cos(a1[i]))
+            #~ self.assertAlmostEqual(q6.sd[i], np.sin(a1[i])*sd1[i])
+            #~ self.assertAlmostEqual(q7[i], np.tan(a1[i]))
+            #~ #self.assertAlmostEqual(q7.sd[i], np.cos(a1[i])**(-2)*sd1[i])
+            #~ self.assertAlmostEqual(q8[i], np.arcsin(a2[i]))
+            #~ #self.assertAlmostEqual(q8.sd[i], sd2[i]/np.sqrt(1-sd2[i]**2))
+            #~ self.assertAlmostEqual(q9[i], np.arccos(a2[i]))
+            #~ #self.assertAlmostEqual(q9.sd[i], sd2[i]/np.sqrt(1-sd2[i]**2))
+            #~ self.assertAlmostEqual(q10[i], np.arctan(a2[i]))
+            #~ #self.assertAlmostEqual(q10.sd[i], sd2[i]*np.cosh(a2[i])**(-2))
+            #~ self.assertAlmostEqual(q11[i], np.sinh(a2[i]))
+            #~ self.assertAlmostEqual(q11.sd[i], sd2[i]*np.cosh(a2[i]))
+            #~ self.assertAlmostEqual(q12[i], np.cosh(a2[i]))
+            #~ self.assertAlmostEqual(q12.sd[i], sd2[i]*np.sinh(a2[i]))
+            #~ self.assertAlmostEqual(q13[i], np.tanh(a2[i]))
+            #~ self.assertAlmostEqual(q13.sd[i], sd2[i]*np.cosh(a2[i])**(-2))
+            #~ self.assertAlmostEqual(q14[i], np.logaddexp(a1[i], a2[i]))
+            #~ self.assertAlmostEqual(q15[i], np.power(np.asarray(q3[i]),np.asarray(q33[i])))
 
 if __name__ == '__main__':
     unittest.main()
