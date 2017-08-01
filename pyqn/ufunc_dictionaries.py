44a8bf2b74184ec1c9717b3982af5be3f3aac055
mayya-bondarevskaya <mayya.bondarevskaya@gmail.com>
2017-08-01 15:32:16 +0100

    add preamble to file

--- pyqn/ufunc_dictionaries.py
+++ pyqn/ufunc_dictionaries.py
@@ -1,3 +1,30 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# ufunc_dictionaries.py
+# A set of predefined error propagation functions and functions which
+# return the resultant units given numpy ufuncs operating on QnArray
+# objects. 
+# 
+# Copyright (C) 2012-2017 Christian Hill
+# Department of Physics and Astronomy, University College London
+# christian.hill@ucl.ac.uk
+#
+# This file is part of PyQn
+#
+# PyQn is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# PyQn is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with PyQn.  If not, see <http://www.gnu.org/licenses/>
+
 from .units import Units, UnitsError
 import numpy as np
 
